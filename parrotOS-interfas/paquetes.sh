b69627ccfe1ecb86c9e32a78a2270d5a22234760
ghost <ghost250624@gmail.com>
2026-03-15 22:41:26 +0000

    Add files via upload

---
+++ parrotOS-interfas/paquetes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "================================"
+echo " Instalando paquetes nesesarios "
+echo "================================"
+
+sleep 3
+echo"esto puede tardar un poco...."
+sleep 5
+
+sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
+
+
