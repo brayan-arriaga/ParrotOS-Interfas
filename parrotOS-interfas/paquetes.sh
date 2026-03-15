83c6c695a4d8f358366c95e328935056f0de2878
ghost <ghost250624@gmail.com>
2026-03-15 22:51:46 +0000

    Refactor paquetes.sh for improved package installation
    Updated package installation script to include necessary libraries and tools for X11/XCB and audio.

--- parrotOS-interfas/paquetes.sh
+++ parrotOS-interfas/paquetes.sh
@@ -4,11 +4,39 @@ set -e
 echo "================================"
 echo " Instalando paquetes nesesarios "
 echo "================================"
+sleep 3
+
+echo "[1] Herramientas basicas del sistema"
+sleep 2
+sudo apt install build-essential git vim
+echo "Herramientas instaladas"
 
 sleep 3
-echo"esto puede tardar un poco...."
-sleep 5
 
-sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
+echo "[2] Lribrerias principales de X11/XCB"
+sleep 2
+sudo apt install libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev 
+echo "librerias principales instaladas"
+
+sleep 3
+
+
+echo "[3] Extennsiones de X11"
+sleep 2
+sudo apt install libxcb-xtest0-dev libxcb-shape0-dev
+echo "Extenciones instaladas"
+
+sleep 3
+
+echo "[4] Audio"
+sudo apt install libasound2-dev 
+echo "Audio instalada" 
+
+sleep 3
+
+echo "====================================="
+echo " INSTALACIONES DE PAQUETES COMPLETAS "
+echo "====================================="
+sleep 3
 
 
