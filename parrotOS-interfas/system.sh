c24b6fbe4a16649b61def29451d5c6cc91c681d8
ghost <ghost250624@gmail.com>
2026-03-17 15:40:17 +0000

    Improve system update script with better messaging

--- parrotOS-interfas/system.sh
+++ parrotOS-interfas/system.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
-
 set -e
 
 echo "=========================================="
 echo "         ACTUALIZANDO SISTEMA             "
 echo "=========================================="
 
-echo "[1] Actualizando  lista de paquetes"
+echo -e "\033[0;35m[1] Actualizando lista de paquetes\033[0m"
 sleep 3
 
-sudo apt update && sudo parrot-upgrade
+#sudo echo "deb https://deb.parrot.sh/parrot rolling main contrib non-free" > /etc/apt/sources.list
 
-echo "Actualizacion correcta"
-sleep 2 
+sudo apt update -y  && sudo parrot-upgrade -y
 
-echo "[2] Actualizando todos los paquetes instalados"
+echo -e "\033[0;35mActualizacion de paquetes correcta\033[0m"
+sleep 2
+
+echo -e "\033[0;35m[2] Actualizando todos los paquetes instalados\033[0m"
 sleep 3
 
 sudo apt full-upgrade -y
 
-echo "Actualizacion correcta"
+echo "\033[0;35mActualizacion correcta\033[0m"
 sleep 2
 
-echo "Limpando paquetes"
+echo -e "\033[0;35m[3] Borrando archivos inecesarios"
 sudo apt autoremove -y
-sudo apt autoclean
+sudo apt autoclean -y
 
-sleep 3 
+sleep 3
 
 echo "==========================================="
 echo "    ACTUALIZACION DE SISTEMA FINALIZADO    "
 echo "==========================================="
 sleep 5
+
 
 
 
