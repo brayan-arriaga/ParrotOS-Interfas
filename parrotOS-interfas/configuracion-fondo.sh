b13b74f7c4a281374989dd055be6a883f92f336e
ghost <ghost250624@gmail.com>
2026-03-17 18:40:30 -0600

    Update wallpaper configuration script for new image

--- parrotOS-interfas/configuracion-fondo.sh
+++ parrotOS-interfas/configuracion-fondo.sh
@@ -7,11 +7,8 @@ echo "====================================="
 echo "[1] configurando fondo de pantalla"
 sleep 3
 
-echo "feh --bg-fill $HOME/Picture/fondo/space.jpg" >> $HOME/.config/bspwm/bspwmrc
+echo "feh --bg-fill $HOME/Pictures/fondo/diseno-de-paisaje-del-espacio_3840x2160_xtrafondos.com.jpg &" >> $HOME/.config/bspwm/bspwmrc
 
-feh --bg-fill $HOME/fondo/espace.jpg 
+feh --bg-fill $HOME/Pictures/fondo/diseno-de-paisaje-del-espacio_3840x2160_xtrafondos.com.jpg 
 
 echo "Configuracion de fondo terminada...."
-
-
-
